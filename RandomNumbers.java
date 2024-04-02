3c1df4797bb5445b223d7b0394602e72cb798874
LeeHyunWon999 <142217859+LeeHyunWon999@users.noreply.github.com>
2024-04-02 20:35:29 +0900

    Update RandomNumbers.java

--- RandomNumbers.java
+++ RandomNumbers.java
@@ -1,6 +1,6 @@
 class RandomNumbers {
   public static void main(String[] args) {
-    Random random = ______;
+    java.util.Random random = ______;
     for(int i = 0; i < 50; i++) {
       // print random numbers
     }
